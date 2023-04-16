4887fd363e933ba29bc54bfc0d76920694c0170f
Meghan Simmons <simmons.meghan@gmail.com>
2023-04-16 14:43:53 -0400

    find needed data from fetch data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,66 @@
-// console.log("Hello! What City do you live in?");
-
 var APIKey = "b640be0d343daf02b19e601754b23be5";
 var city = "Atlanta";
 // var state = "GA";
-var country = "US";
-var cityLatA = 44.34;
-var cityLongB = 10.99;
+// var country = "US";
+var cityLat = "";
+var cityLong = "";
 
-// var currentWeather = 
 
-fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + country + "&limit=5&appid=" + APIKey)
+fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + APIKey) //Fetch city coordinates based on city entered
 .then(function(response){
     return response.json();
     })
 .then(function(data){
-    console.log(data);
-    var cityLat = data[0].lat;
-    var cityLong = data[0].lon;
-    // console.log(cityLat);
-    // console.log(cityLong);
-    cityDataLatLong(cityLat, cityLong);
+    // console.log(data);
+    cityLat = data[0].lat;  //city lattitude
+    cityLong = data[0].lon;     //city longitute
+    console.log(cityLat);
+    console.log(cityLong);
+
+    cityCurrent(cityLat, cityLong); //call cityCurrent function to get current forecast
+    cityFiveDay(cityLat, cityLong); //call cityFiveDay function to get 5 day forecast
 })
 
 
-// fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + state + "," + country + "&limit=5&appid=" + APIKey)
-//     .then(function(response){
-//         return response.json();
-//     })
-//     .then(function(data){
-//         console.log(data);
-//     })
-
-
-function cityDataLatLong(lat, long) {
-    fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid="+APIKey)
+function cityCurrent(lat, long) { //Fetch current weather data
+    fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=" + APIKey + "&units=imperial")
     .then(function(response){
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
+        console.log(data.main.temp); //object returned
+        console.log(data.main.humidity); 
+        console.log(data.weather[0].icon); 
+        console.log(data.wind.speed); 
+        console.log(data.name); 
+
     })
-};
+}
+
+function cityFiveDay (lat, long) { //Fetch 5 day forecast weather data at noon timestamp
+    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&appid=" + APIKey  +"&units=imperial")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function (data) {
+        // for (let index = 0; index < array.length; index++) {
+        //     const element = array[index];
+
+        //     if (condition) {
+            
+        //     } else {
+                
+        //     }
+            
+        // }
+        
+        console.log(data.list);     // all forecast time points
+        console.log(data.list[0]);      // first forecast time points
+        console.log(data.list[0].dt_txt);       // date and time
+        console.log(data.list[0].main.humidity);        // humidity
+        console.log(data.list[0].main.temp);        // temp
+        console.log(data.list[0].weather[0].icon);      // weather icon
+        console.log(data.list[0].wind.speed);       // wind speed
+    })
+}
+
